7a57d725e356f6ab3e42ca19229dda590b4a54ff
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-05-05 20:38:27 -0600

    Commit para subir ejercicio elaborado con Spring Data.

---
+++ ob-spring-datajpa/src/main/java/com/arthycode/obspringdatajpa/ObSpringDatajpaApplication.java
@@ -0,0 +1,28 @@
+package com.arthycode.obspringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class ObSpringDatajpaApplication {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = SpringApplication.run(ObSpringDatajpaApplication.class, args);
+
+		CocheRepository repository = context.getBean(CocheRepository.class);
+
+		System.out.println("El num de coches en base de datos es: " + repository.count());
+
+		// crear y almacenar un coche en base de datos
+		Coche toyota = new Coche(null, "Toyota", "Prius", 2010);
+		repository.save(toyota);
+
+		System.out.println("El num de coches en base de datos es: " + repository.count());
+
+		// recuperar todos
+		System.out.println(repository.findAll());
+	}
+
+}
