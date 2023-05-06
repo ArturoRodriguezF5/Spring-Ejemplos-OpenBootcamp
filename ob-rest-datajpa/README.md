11d344531ff5c13775d0829775f6765e41c11bfd
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-05-06 12:34:36 -0600

    Commit para subir avance de ejercicio hecho en Spring con JPA y REST.

---
+++ ob-rest-datajpa/README.md
@@ -0,0 +1,16 @@
+## Spring Boot
+
+Proyecto Spring Boot con las dependencias / Starters:
+* H2
+* Spring Web
+* Spring Data JPA
+* Spring Boot Tools
+
+Aplicación REST API con acceso a base de datos H2 para persistir la información.
+
+El acceso se puede realizar desde Postman o Navegador.
+
+## Entidad book
+1. Book
+2. BookRepository
+3. BookController
---
+++ ob-rest-datajpa/src/main/java/com/arthycode/obrestdatajpa/ObRestDatajpaApplication.java
@@ -0,0 +1,34 @@
+package com.arthycode.obrestdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.time.LocalDate;
+
+@SpringBootApplication
+public class ObRestDatajpaApplication {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = SpringApplication.run(ObRestDatajpaApplication.class, args);
+		BookRepository repo = context.getBean(BookRepository.class);
+
+		// Crear un libro
+		Book book = new Book(null, "Homo Deus", "yuval Noah", 450, 29.99,
+				LocalDate.of(2018, 12, 1), true);
+		Book book2 = new Book(null, "Homo Sapiens", "yuval Noah", 410, 19.99,
+				LocalDate.of(2013, 12, 1), true);
+		System.out.println("Hay: " + repo.count() + " libros en el repo.");
+		// Almacenar un libro
+		repo.save(book);
+		repo.save(book2);
+		// Recuperar todos los libros
+		System.out.println(repo.findAll());
+		System.out.println("Hay: " + repo.count() + " libros en el repo.");
+		// Borrar un libro
+		repo.deleteById(1L);
+		System.out.println(repo.findAll());
+	}
+
+}
