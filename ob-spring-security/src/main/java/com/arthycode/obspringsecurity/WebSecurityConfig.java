2d881580508f327ab9cb8a2bdb384c5028538b0b
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-05-23 21:43:34 -0600

    Commit para subir ejemplo de Spring Security.

---
+++ ob-spring-security/src/main/java/com/arthycode/obspringsecurity/WebSecurityConfig.java
@@ -0,0 +1,44 @@
+package com.arthycode.obspringsecurity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var user = User.withUsername("arthycode")
+                .password("51arthycode")
+                .roles("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/hola");
+    }
+
+}
---
+++ ob-spring-security/src/test/java/com/arthycode/obspringsecurity/ObSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthycode.obspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
